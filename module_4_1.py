cd88afc2e09814d148745fccc8f62f0b2ddc99a7
Ultragenik <ultragenik@gmail.com>
2024-12-15 18:28:36 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import fake_divide as we
+from true_math import true_divide as ew
+
+result1 = we(69, 3)
+result2 = we(3, 0)
+result3 = ew(49, 7)
+result4 = ew(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
